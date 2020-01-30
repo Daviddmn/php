141ecd2f9c5ffe30354b2a125a14d0ab53fe5e37
David <51552786+Daviddmn@users.noreply.github.com>
2020-01-30 06:40:48 -0800

    Create data-types.php

---
+++ data-types.php
@@ -0,0 +1,32 @@
+// მონაცემთა ტიპები : String, Integer, Float, Boolean, Array, Object, NULL, Resource
+
+// სტრინგი არის უბრალოდ ტექსტი, ტექსტი თავსდება პჭყალებში "" და ''
+//ინტეჯერი(10) და ფლოატი(10.2) არის ციფრები, ვწერთ პჭყალების გრეშე
+
+<?php
+$x = "Hello world!"; //სტინგ
+$y = 'Hello world!';
+$nn = 10; //integer
+$mm = 10.2; //float
+
+echo $x;
+echo "<br>";
+echo $y;
+?>
+
+//boolean TRUE FALSE
+
+//NULL, არის მონაცემთა ტიპი , რომელსაც არ გააჩნია არანაირი ღირებულება
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = "Hello world!";
+$x = null;
+var_dump($x);
+?>
+
+</body>
+</html>
