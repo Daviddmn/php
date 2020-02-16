a3018bb33d453cdd25595bc7bc19273e23727d41
David <51552786+Daviddmn@users.noreply.github.com>
2020-02-16 10:15:32 -0800

    Create While-loop.php

---
+++ While-loop.php
@@ -0,0 +1,20 @@
+
+/*   სანამ (მსგომარეობა არის ჭეშმარიტი) {გაეშვას კოდი}
+Syntax
+while (condition is true) {
+    code to be executed;
+}
+*/
+
+<?php
+// სანამ $x არ ნაკლებია 5 ზე მოხდეს $x ის ინკრემენტი ასევე ექო
+
+$x = 1;
+
+while($x <= 5) {
+    echo "The number is: $x <br>";
+    $x++;
+}
+
+
+?>
